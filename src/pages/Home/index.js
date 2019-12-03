123684a825369874d850cd8adbc71f41c11e9691
Wolgrand Neto <wolgrandneto@MacBook-Air-de-Wolgrand.local>
2019-12-03 06:30:11 -0500

    Add Hooks to Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { MdAddShoppingCart } from 'react-icons/md';
@@ -9,31 +9,32 @@ import * as CartActions from '../../store/modules/cart/actions';
 
 import { ProductList } from './styles';
 
-class Home extends Component {
-  state = {
-    products: [],
-  };
+function Home ({ amount, addToCartRequest }) {
+  const [products, setProducts] = useState([]);
 
-  async componentDidMount() {
-    const response = await api.get('products');
+  useEffect(() => {
+    async function loadProducts() {
+      const response = await api.get('products');
 
     const data = response.data.map(product => ({
       ...product,
       priceFormatted: formatPrice(product.price),
     }));
+      setProducts(data);
 
-    this.setState({ products: data });
-  }
+    }
 
-  handleAddProduct = id => {
-    const { addToCartRequest } = this.props;
+    loadProducts();
+  }, []);
 
+
+
+  function handleAddProduct(id) {
     addToCartRequest(id);
   };
 
-  render() {
-    const { products } = this.state;
-    const { amount } = this.props;
+
+
 
     return (
       <ProductList>
@@ -46,7 +47,7 @@ class Home extends Component {
 
             <button
               type="button"
-              onClick={() => this.handleAddProduct(product.id)}
+              onClick={() => handleAddProduct(product.id)}
             >
               <div>
                 <MdAddShoppingCart size={16} color="#fff" />{' '}
@@ -60,7 +61,7 @@ class Home extends Component {
       </ProductList>
     );
   }
-}
+
 
 const mapStateToProps = state => ({
   amount: state.cart.reduce((amount, product) => {
